6f8153dc461c1dc872db7ca9bf16d690d0e011da
sadicangel <sadicangel@hotmail.com>
2024-10-22 18:05:15 +0100

    Adds initial code.

---
+++ src/DotNetDotEnv/DotEnv.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace DotNetDotEnv;
+
+public sealed record class DotEnv : IReadOnlyDictionary<string, string>
+{
+    private readonly Dictionary<string, string> _values;
+
+    public DotEnv() => _values = [];
+
+    public string this[string key] { get => _values[key]; init => _values[key] = VariableName.ThrowIfInvalid(value); }
+
+    IEnumerable<string> IReadOnlyDictionary<string, string>.Keys => _values.Keys;
+    IEnumerable<string> IReadOnlyDictionary<string, string>.Values => _values.Values;
+
+    public int Count => _values.Count;
+
+    public bool ContainsKey(string key) => _values.ContainsKey(key);
+
+    public IEnumerator<KeyValuePair<string, string>> GetEnumerator() => _values.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_values).GetEnumerator();
+
+    public bool TryGetValue(string key, [MaybeNullWhen(false)] out string value) => _values.TryGetValue(key, out value);
+}
+
+internal static partial class VariableName
+{
+    [GeneratedRegex(@"^[a-zA-Z_]+[a-zA-Z0-9_]*$")]
+    private static partial Regex GetOrCreateVariableNameRegex();
+
+    public static string ThrowIfInvalid(string value)
+    {
+        var regex = GetOrCreateVariableNameRegex();
+        if (!regex.IsMatch(value))
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(value),
+                value,
+                $"Invalid variable name. Must match the following regex pattern: {regex}");
+        }
+
+        return value;
+    }
+}
---
+++ tests/DotNetDotEnv.Tests/VariableNameTests.VariableName.cs
@@ -0,0 +1,33 @@
+﻿namespace DotNetDotEnv.Tests;
+
+public partial class VariableNameTests
+{
+    [Theory]
+    [MemberData(nameof(ValidVariableNames))]
+    public void Allow_when_value_is(string value) =>
+        Assert.Equal(value, VariableName.ThrowIfInvalid(value));
+
+    [Theory]
+    [MemberData(nameof(InvalidVariableNames))]
+    public void Throw_when_value_is(string value) =>
+        Assert.Throws<ArgumentOutOfRangeException>(() => VariableName.ThrowIfInvalid(value));
+
+    public static TheoryData<string> ValidVariableNames() => [
+        "DATABASE",
+        "APP_NAME",
+        "_NAME",
+        "RELEASE2024",
+        "AppConfig",
+        "appSettings"
+    ];
+
+    public static TheoryData<string> InvalidVariableNames() => [
+        "",
+        "app-name",
+        "1INVALID_KEY",
+        "USER@NAME",
+        "PASSWORD#123",
+        "APP_NAME ",
+        "  APP_NAME",
+    ];
+}
